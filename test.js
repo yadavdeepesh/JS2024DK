4f9707a3cc533c5c8bd59a5a493efd362a84640b
deepesh yadav <yadavdeepesh13@gmail.com>
2023-12-24 15:49:30 +0530

    datatype in js

--- test.js
+++ test.js
@@ -1,8 +1,72 @@
-console.log("hello world");
+// console.log("hello world");
 
 /* section 2 value and variable
 */
-var myName = "Deepesh";
-var myAge = 25;
-console.log(myAge);
-console.log(myName);
+// var myName = "Deepesh";
+// var myAge = 25;
+// console.log(myAge);
+// console.log(myName);
+
+// datatypes
+
+/*
+var x ;
+console.log(typeof(x));
+
+var y = false;
+console.log(typeof(y));
+
+var name = "Khushi";
+console.log(typeof(name));
+
+var age = 45;
+console.log(typeof(age));
+*/
+
+/*
+
+x = 10 + "20";
+
+console.log(x);
+console.log(typeof(x));
+console.log("+++++++++++++++++++");
+y = 9 - "5";
+console.log(y);
+console.log(typeof(y));
+console.log("+++++++++++++++++++");
+z = "java" + "script";
+console.log(z);
+console.log(typeof(z));
+console.log("+++++++++++++++++++");
+p = " " + " ";
+console.log(p);
+console.log(typeof(p));
+
+console.log("+++++++++++++++++++");
+q = " " + 0;
+console.log(q);
+console.log(typeof(q));
+*/
+console.log("+++++++++++++++++++");
+q = true + true;
+console.log(q);
+console.log(typeof(q));
+
+console.log("+++++++++++++++++++");
+q = true + false;
+console.log(q);
+console.log(typeof(q));
+
+
+console.log("+++++++++++++++++++");
+q = false + true;
+console.log(q);
+console.log(typeof(q));
+
+console.log("+++++++++++++++++++");
+q = false - true;
+console.log(q);
+console.log(typeof(q));
+
+
+
