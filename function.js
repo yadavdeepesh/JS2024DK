d5db78f674a4951be47c84816c2baed716fe7cc8
deepesh yadav <yadavdeepesh13@gmail.com>
2024-02-17 20:31:48 +0530

    using function in js

---
+++ function.js
@@ -0,0 +1,62 @@
+// create a function in js
+
+// function sum(){
+//     var a = 10, b=20;
+//     var total = a + b;
+//     console.log(total);
+// }
+
+// // callig a function 
+
+// sum();
+
+// function parameter and  function arguments
+
+// function sum(a,b){// that is the function parameter 
+//     var total = a + b;
+//     console.log(total);
+// }
+// sum();//NaN
+// sum(29,23);// output :52 // that is the function arguments
+// sum(33,45);// output :78
+
+
+
+// function sum(a,b){// that is the function parameter 
+//     var total = a + b;
+//     console.log(total);
+// }
+
+
+// var funExp = sum(5,15);
+
+// return keyword in js
+
+// function sum(a,b){// that is the function parameter 
+//     return  a + b;
+// }
+
+
+// var funExp = sum(50,15);
+
+// console.log("sum of two number is : "+funExp);
+
+
+// Anonymous function 
+
+var funExp = function(a,b){
+    return total = a + b;
+}
+
+console.log(funExp(5,15));
+
+var sum = funExp(15,15);
+var sum1 = funExp(25,15);
+
+console.log("the sum of two number " +sum );
+
+console.log("the sum of two number " +sum );
+
+console.log(sum>sum1);
+
+console.log(sum1>sum);
