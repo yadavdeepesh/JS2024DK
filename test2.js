f1d805674f7018236be3846e96358bcac9e5df85
deepesh yadav <yadavdeepesh13@gmail.com>
2024-02-17 19:32:11 +0530

    using  variable in js

---
+++ test2.js
@@ -0,0 +1,15 @@
+// swap 2 number without using third variable
+var a = 5;
+var b = 6;
+console.log("Before swapimg ++++++++++++++");
+console.log("a ="+a);
+console.log("b ="+b);
+console.log("After swapimg ++++++++++++++");
+a = a + b;
+b = a - b ;
+a = a - b;
+console.log("a ="+a);
+console.log("b ="+b);
+// sol 
+// a = 6 
+// b = 5;
