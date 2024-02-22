abca0a8a47b3f1acbe46ccf24488ad72b9defb96
deepesh yadav <yadavdeepesh13@gmail.com>
2024-02-22 23:27:01 +0530

    using variable in JS

---
+++ variable.js
@@ -0,0 +1,80 @@
+// var name  = "eshu ";
+// name ="Dk";
+// console.log(name);
+
+// let name1 = "eshu ";
+// name1 ="DkY";
+// console.log(name1);
+
+
+// const name2  = "eshu ";
+// name2 ="Dk";
+// console.log(name2);
+
+// using var for creating a varaiable
+
+// function bioData(){
+//     var firstname = "Deepesh";
+//     console.log(firstname);
+
+//     if(true){
+//         var mylastname = "Yadav";
+//         console.log("inner " + firstname);
+//         console.log("inner " + mylastname);
+//     }
+//     console.log("in function  " + firstname);
+// console.log("in function " + mylastname);
+// }
+
+// bioData();
+
+// console.log("outter " + firstname);
+// console.log("outter " + mylastname);
+
+
+
+// using let for creating a varaiable
+/*
+function bioData(){
+    let firstname = "Deepesh";
+    console.log(firstname);
+
+    if(true){
+        let mylastname = "Yadav";
+        console.log("inner " + firstname);
+        console.log("inner " + mylastname);
+    }
+    console.log("in function  " + firstname);
+console.log("in function " + mylastname);
+}
+
+bioData();
+
+console.log("outter " + firstname);
+console.log("outter " + mylastname);
+*/
+
+// using let for creating a const
+/*
+function bioData(){
+    const firstname = "Deepesh";
+    console.log(firstname);
+
+    if(true){
+        const mylastname = "Yadav";
+        console.log("inner " + firstname);
+        console.log("inner " + mylastname);
+    }
+    console.log("in function  " + firstname);
+console.log("in function " + mylastname);
+}
+
+bioData();
+
+console.log("outter " + firstname);
+console.log("outter " + mylastname);
+*/
+// NOTE ::
+
+// var => function scope
+// let and const => block scope
