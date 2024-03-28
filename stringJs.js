25c77311e56d0976870ad753deba31ca5c367661
deepesh yadav <yadavdeepesh13@gmail.com>
2024-03-28 13:22:11 +0530

    using the string method in js

---
+++ stringJs.js
@@ -0,0 +1,73 @@
+// String 
+// A javascript string is zero or more characters written inside quotes.
+
+
+// let myName  = 'Deepesh Yadav';
+// let myProfession = "Software Engineer";
+// let ytname = new String("thapa technical");
+// // this way not use yet
+
+// console.log(myName);
+// console.log(myProfession);
+// console.log(ytname);
+
+// how find the length of string 
+// length the property , not method 
+
+// let paragraph = "my name  is deepesh . my hobbies is watching cricket maths and playing cricket , and my dreame have one girl friend and very good job";
+
+// console.log(paragraph.length);
+
+// Escape Character 
+
+// let anySentense =" i am liking punjabi girls and cute girls , \"i am like wamiqa gabbi\"  ";
+// you can use single quote inside the double quotes and vise -versa 
+// let anySentense =" i am liking punjabi girls and cute girls , 'i am like wamiqa gabbi'  ";
+
+// you can use double quote inside the single quotes and vise -versa 
+// let anySentense =' i am liking punjabi girls and cute girls , "i am like wamiqa gabbi"  ';
+// console.log(anySentense);
+
+// Finding a string in a string 
+
+// let myBioData = "I am Deepesh yadav from Muzaffarnagr and father name is vijay yadav ";
+
+// console.log(myBioData.indexOf("yadav",18));
+// console.log(myBioData.length);
+
+// console.log(myBioData.lastIndexOf("yadav"));
+
+// // Search Method 
+// // do not take second parameter
+
+// let sData = myBioData.search("vijay");
+
+// console.log(sData);
+
+// JavaScript String slice()
+/*
+Description
+The slice() method extracts a part of a string.
+
+The slice() method returns the extracted part in a new string.
+
+The slice() method does not change the original string.
+
+The start and end parameters specifies the part of the string to extract.
+
+The first position is 0, the second is 1, ...
+*/
+
+// let text = "Hello world!";
+// // let result = text.slice(0,7);
+// let result = text.slice(3);
+
+// console.log(result);
+
+let myTweet = "my name is deepesh yadav from nagaland";
+
+// console.log(myTweet.slice(-1));
+
+// substring() method in JS
+
+
